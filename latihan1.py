52e0d1f275498762029920c46ea1c6030b7be0d8
udinajaokeee <148648263+udinajaokeee@users.noreply.github.com>
2023-10-27 19:35:40 +0700

    Create latihan1.py

---
+++ latihan1.py
@@ -0,0 +1,40 @@
+1.
+a = 55
+b = 25
+print("a :",a)
+print("b :",b)
+print("hasil a+b =",a+b)
+print("hasil a-b =",a-b)
+
+2.
+a = 15
+b = 10
+nilai1 = a*b
+nilai2 = a+b
+print("a : a")
+print("b : b")
+print(nilai1/nilai2)
+
+3.
+a = 50
+b = 25
+t = 10
+nilai = a+b
+print("a :",a)
+print("b :",b)
+print("t :",t)
+print("luas trapesium : 1/2 x (a+b) x t, hasil: ",0.5*nilai*t)
+
+4.
+r = 65
+pi = 3.14
+print("r :",r)
+print("pi :",pi)
+print("rumus keliling lingkaran: 2 x pi x r, hasil :",2*pi*r)
+
+5.
+r = 22
+pi = 3.14
+print("r : ",r)
+print("pi : ",pi)
+print("luas lingkaran : pi x 2r, hasil =",pi*r*r)
